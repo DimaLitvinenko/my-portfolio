b585769cf75eb88edc966b3138cbf2be31be8a55
DimaLitvinenko <80059894+DimaLitvinenko@users.noreply.github.com>
2021-10-18 20:45:21 +0300

    Updates

---
+++ contacts.a5e8e6da813b013b2a96.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";document.addEventListener("DOMContentLoaded",(function(){console.log("DOMContentLoaded","page-contacts")}))})();
+//# sourceMappingURL=contacts.a5e8e6da813b013b2a96.js.map
