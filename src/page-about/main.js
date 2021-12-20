399fa64523e480505ba10f40265c6141a5cce88a
DimaLitvinenko <dimaly.too@gmail.com>
2021-12-20 17:27:19 +0200

    update

--- src/page-about/main.js
+++ src/page-about/main.js
@@ -1,9 +1,8 @@
 import '../scss/main.scss'
 import './page.scss'
-import img from '../img/unicorn.jpg'
+// import img from '../img/unicorn.jpg'
 
-
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOMContentLoaded', 'page-about')
-  console.log('Image through require()', img)
-})
+// document.addEventListener('DOMContentLoaded', () => {
+// console.log('DOMContentLoaded', 'page-about')
+// console.log('Image through require()', img)
+// })
