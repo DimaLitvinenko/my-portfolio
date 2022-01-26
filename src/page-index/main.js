dd42c3b99859b690070f47ef32991716d6139093
DimaLitvinenko <dimaly.too@gmail.com>
2022-01-26 22:05:23 +0200

    added images, pages, styles

--- src/page-index/main.js
+++ src/page-index/main.js
@@ -10,6 +10,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
 window.Foo = Foo.instance()
 window.Bar = Bar()
+// console.log(window.Foo.getValue())
+// console.log(window.Bar)
 
-console.log(window.Foo.getValue())
-console.log(window.Bar)
+const menuToggle = document.querySelector('.menu-toggle')
+const bxMenu = document.querySelector('.bx-menu')
+const bxX = document.querySelector('.bx-x')
+const navBar = document.querySelector('.navbar')
+
+// --- open menu ---
+bxMenu.addEventListener('click', e => {
+  if (e.target.classList.contains('bx-menu')) {
+    navBar.classList.add('show-navbar')
+    bxMenu.classList.add('hide-bx')
+    bxX.classList.add('show-bx')
+  }
+})
+
+// --- close menu ---
+bxX.addEventListener('click', e => {
+  if (e.target.classList.contains('bx-x')) {
+    navBar.classList.remove('show-navbar')
+    bxMenu.classList.remove('hide-bx')
+    bxX.classList.remove('show-bx')
+  }
+})
