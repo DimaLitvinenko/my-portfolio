ae81e3ea6b202822992172ac6037fbcc6e3f7d58
DimaLitvinenko <dimaly.too@gmail.com>
2022-10-09 17:11:07 +0300

    logo added

--- src/page-index/main.js
+++ src/page-index/main.js
@@ -1,5 +1,6 @@
 import '../scss/main.scss'
 import './page.scss'
+import '../img/logo_angry-dog.png'
 import { Bar } from '../js/bar.es6'
 const { Foo } = require('../js/foo.cjs')
 // import 'normalize.css/normalize.css'
@@ -13,7 +14,7 @@ window.Bar = Bar()
 // console.log(window.Foo.getValue())
 // console.log(window.Bar)
 
-const menuToggle = document.querySelector('.menu-toggle')
+// const menuToggle = document.querySelector('.menu-toggle')
 const bxMenu = document.querySelector('.bx-menu')
 const bxX = document.querySelector('.bx-x')
 const navBar = document.querySelector('.navbar')
